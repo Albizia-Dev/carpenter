64b9bc4ddb5738fb855917093493b9e9fa4a6189
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-08-28 18:15:32 +0700

    refactor: make loader size root-relative

--- lib/src/components/basic/loader.dart
+++ lib/src/components/basic/loader.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 
+import 'package:carpenter_units/carpenter_units.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../foundation/roles.dart';
@@ -9,12 +10,12 @@ import '../../foundation/theme.dart';
 final class CarpenterLoader extends StatefulWidget {
   const CarpenterLoader({
     super.key,
-    this.size = 24,
+    this.size = const Rem(1.5),
     this.strokeWidth = 2.5,
     this.semanticLabel = 'Loading',
   });
 
-  final double size;
+  final LengthUnit size;
   final double strokeWidth;
   final String semanticLabel;
 
@@ -50,6 +51,7 @@ final class _CarpenterLoaderState extends State<CarpenterLoader>
   @override
   Widget build(BuildContext context) {
     final theme = CarpenterTheme.of(context);
+    final extent = context.units(widget.size);
     final accent = theme.actions
         .resolve(
           ActionColorRole.primary,
@@ -61,7 +63,7 @@ final class _CarpenterLoaderState extends State<CarpenterLoader>
       label: widget.semanticLabel,
       liveRegion: true,
       child: SizedBox.square(
-        dimension: widget.size,
+        dimension: extent,
         child: AnimatedBuilder(
           animation: _controller,
           builder: (context, _) => Transform.rotate(
