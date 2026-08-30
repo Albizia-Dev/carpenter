78e94abcc0b62bff9013c6ef4ff61646010e063c
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-08-31 05:58:03 +0700

    chore: clean time input imports

--- lib/src/components/basic/input/time_input.dart
+++ lib/src/components/basic/input/time_input.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 
 import '../../../foundation/roles.dart';
 import '../../behaviour/dialog.dart';
-import '../button/button.dart';
 import '../icons.dart';
 import 'adaptive_picker.dart';
 import 'masked_input.dart';
