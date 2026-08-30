178365a9f5b782face1aba4cab0e43769840175d
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-08-30 15:34:39 +0700

    Loosen adaptive layout test constraints

--- test/layout/action_overflow_test.dart
+++ test/layout/action_overflow_test.dart
@@ -70,13 +70,16 @@ void main() {
     await tester.pumpWidget(
       Directionality(
         textDirection: TextDirection.ltr,
-        child: SizedBox(
-          width: 300,
-          child: ActionOverflowLayout(
-            gap: 8,
-            minimumInlineActionWidth: 100,
-            content: const SizedBox(width: 260, height: 20),
-            actions: const SizedBox(width: 200, height: 20),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: SizedBox(
+            width: 300,
+            child: ActionOverflowLayout(
+              gap: 8,
+              minimumInlineActionWidth: 100,
+              content: const SizedBox(width: 260, height: 20),
+              actions: const SizedBox(width: 200, height: 20),
+            ),
           ),
         ),
       ),
@@ -91,13 +94,16 @@ void main() {
     await tester.pumpWidget(
       Directionality(
         textDirection: TextDirection.ltr,
-        child: SizedBox(
-          width: 300,
-          child: ActionOverflowLayout(
-            gap: 8,
-            minimumInlineActionWidth: 40,
-            content: const SizedBox(width: 180, height: 20),
-            actions: const SizedBox(width: 100, height: 20),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: SizedBox(
+            width: 300,
+            child: ActionOverflowLayout(
+              gap: 8,
+              minimumInlineActionWidth: 40,
+              content: const SizedBox(width: 180, height: 20),
+              actions: const SizedBox(width: 100, height: 20),
+            ),
           ),
         ),
       ),
