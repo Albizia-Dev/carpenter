039d2b5c37a1a4efed15a789246ddba1c3b4bfa3
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-09-05 19:51:00 +0700

    api: add layout entrypoint

---
+++ lib/layout.dart
@@ -0,0 +1,36 @@
+/// Carpenter collection views plus adaptive application and page layout.
+library;
+
+export 'collections.dart';
+
+export 'src/components/layout/app_frame.dart';
+export 'src/components/layout/application_shell.dart';
+export 'src/components/layout/master_detail.dart';
+export 'src/components/layout/page_header.dart';
+export 'src/components/layout/patterns/block_group.dart';
+export 'src/components/layout/patterns/collection_page.dart';
+export 'src/components/layout/patterns/form_page.dart';
+export 'src/components/layout/patterns/header_actions.dart';
+export 'src/components/layout/patterns/list_report.dart';
+export 'src/components/layout/patterns/master_detail_page.dart';
+export 'src/components/layout/patterns/object_page.dart';
+export 'src/components/layout/patterns/page_blocks.dart'
+    hide CarpenterBlockGroup;
+export 'src/components/layout/patterns/page_body.dart';
+export 'src/components/layout/patterns/page_section.dart';
+export 'src/components/layout/patterns/states/empty_state.dart';
+export 'src/components/layout/regions/adaptive_region.dart';
+export 'src/components/layout/regions/adaptive_region_policy.dart';
+export 'src/components/layout/regions/detail_region.dart';
+export 'src/components/layout/regions/navigation_region.dart';
+export 'src/components/layout/regions/primary_region.dart';
+export 'src/components/layout/regions/region_presentation.dart';
+export 'src/components/layout/regions/region_role.dart';
+export 'src/components/layout/regions/secondary_region.dart';
+export 'src/components/layout/restorable_split.dart';
+export 'src/components/layout/root_layout.dart';
+export 'src/components/layout/section_layout.dart';
+export 'src/components/layout/sidebar.dart';
+export 'src/components/layout/split_view.dart';
+export 'src/components/layout/tabs_layout.dart';
+export 'src/components/layout/toolbar.dart';
