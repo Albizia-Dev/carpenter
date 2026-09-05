caf330e36994c2e489fd7e7908023bd984255109
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-09-05 19:50:36 +0700

    api: add components entrypoint

---
+++ lib/components.dart
@@ -0,0 +1,64 @@
+/// Carpenter foundation plus reusable basic and behavioural UI components.
+library;
+
+export 'foundation.dart';
+export 'gravity_icons.dart';
+
+// Basic components.
+export 'src/components/basic/avatar.dart';
+export 'src/components/basic/avatar_group.dart';
+export 'src/components/basic/badge.dart';
+export 'src/components/basic/button/button.dart';
+export 'src/components/basic/button/icon_button.dart';
+export 'src/components/basic/button/toggle_button.dart';
+export 'src/components/basic/calendar.dart';
+export 'src/components/basic/card.dart';
+export 'src/components/basic/checkbox.dart';
+export 'src/components/basic/color_picker.dart';
+export 'src/components/basic/icon.dart';
+export 'src/components/basic/icons.dart';
+export 'src/components/basic/input/date_input.dart';
+export 'src/components/basic/input/date_range_input.dart';
+export 'src/components/basic/input/field_shell.dart';
+export 'src/components/basic/input/file_input.dart';
+export 'src/components/basic/input/input.dart';
+export 'src/components/basic/input/masked_input.dart';
+export 'src/components/basic/input/number_input.dart';
+export 'src/components/basic/input/text_area.dart';
+export 'src/components/basic/input/time_input.dart';
+export 'src/components/basic/link.dart';
+export 'src/components/basic/loader.dart';
+export 'src/components/basic/progress.dart';
+export 'src/components/basic/radio.dart';
+export 'src/components/basic/radio_group.dart';
+export 'src/components/basic/select/async_autosuggest.dart';
+export 'src/components/basic/select/autosuggest.dart';
+export 'src/components/basic/select/combo_box.dart';
+export 'src/components/basic/select/select.dart';
+export 'src/components/basic/select/selection_button_group.dart';
+export 'src/components/basic/status_indicator.dart';
+export 'src/components/basic/switch.dart';
+export 'src/components/basic/tag.dart';
+export 'src/components/basic/text.dart';
+
+// Behaviour and transient surfaces.
+export 'src/components/behaviour/control.dart';
+export 'src/components/behaviour/dialog.dart';
+export 'src/components/behaviour/drag_and_drop/drag_operation.dart';
+export 'src/components/behaviour/drag_and_drop/drag_payload.dart';
+export 'src/components/behaviour/drag_and_drop/drag_scope.dart';
+export 'src/components/behaviour/drag_and_drop/draggable.dart';
+export 'src/components/behaviour/drag_and_drop/drop_target.dart';
+export 'src/components/behaviour/expander.dart';
+export 'src/components/behaviour/menu/dropdown.dart';
+export 'src/components/behaviour/menu/menu.dart';
+export 'src/components/behaviour/menu/menu_entry.dart';
+export 'src/components/behaviour/notice.dart';
+export 'src/components/behaviour/popover.dart';
+export 'src/components/behaviour/toast/toast.dart';
+export 'src/components/behaviour/toast/toast_region.dart';
+export 'src/components/behaviour/toast/toaster_controller.dart';
+export 'src/components/behaviour/tooltip.dart';
+
+// Content primitives.
+export 'src/components/content/inspector.dart';
