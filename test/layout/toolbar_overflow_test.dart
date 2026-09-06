ef4c3ed59d23e0031b568010da42a17fa8b4fc2e
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-09-06 19:02:05 +0700

    test: cover toolbar overflow interaction

---
+++ test/layout/toolbar_overflow_test.dart
@@ -0,0 +1,44 @@
+import 'package:carpenter/carpenter.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../helpers/harness.dart';
+
+void main() {
+  testWidgets('overflow menu opens without changing toolbar geometry', (
+    tester,
+  ) async {
+    var invocations = 0;
+    await tester.pumpWidget(
+      carpenterOverlayHarness(
+        SizedBox(
+          width: 56,
+          child: CarpenterToolbar(
+            items: [
+              CarpenterToolbarItem(
+                group: CarpenterToolbarGroup.overflow,
+                action: CarpenterActionDescriptor(
+                  id: 'archive',
+                  label: 'Archive',
+                  onInvoke: () => invocations += 1,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    final before = tester.getSize(find.byType(CarpenterToolbar));
+    await tester.tap(find.bySemanticsLabel('More actions'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Archive'), findsOneWidget);
+    expect(tester.getSize(find.byType(CarpenterToolbar)), before);
+
+    await tester.tap(find.text('Archive'));
+    await tester.pumpAndSettle();
+    expect(invocations, 1);
+    expect(tester.takeException(), isNull);
+  });
+}
