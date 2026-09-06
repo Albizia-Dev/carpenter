4cad00a7cf4760261449aecef748fe68c35f29c6
Nikolay Chupin <146885777+njxxdev@users.noreply.github.com>
2026-09-06 09:49:57 +0700

    docs: mark resource controller as extension point

--- lib/src/page/resource.dart
+++ lib/src/page/resource.dart
@@ -24,6 +24,7 @@ final class CarpenterResourceLoadRequest {
 typedef CarpenterResourceLoader<T> =
     Future<T> Function(CarpenterResourceLoadRequest request);
 
+/// Extensible resource lifecycle base for application-specific controllers.
 class CarpenterResourceController<T>
     extends ValueNotifier<CarpenterPageState>
     implements CarpenterPageController {
